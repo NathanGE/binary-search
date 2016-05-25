230b97df01e2fb65688594a51b97cd455e085256
Nathan <nathangelliott@gmail.com>
2016-05-25 14:13:29 +0000

    binary search of a sorted array in Ruby

---
+++ binary_search.rb
@@ -0,0 +1,21 @@
+# Binary search on a sorted array.
+
+def binary_search(array, target)
+  min = 0
+  max = array.length - 1
+  while max >= min
+    guess = (max + min) / 2
+    if array[guess] == target
+      return guess
+    elsif array[guess] < target
+      min = guess + 1
+    else
+      max = guess - 1
+    end
+  end
+  -1
+end
+
+index_of_target = binary_search([1, 3, 5, 6, 12, 14], 6)
+
+puts "The index of the target is: #{index_of_target}"
